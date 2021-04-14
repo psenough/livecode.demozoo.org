eae4714bdc4b93a8bfb53d1579abc1c560435a7a
totetmatt <matthieu.totet@gmail.com>
2021-04-15 00:30:53 +0200

    add started to display event per date

--- bin/generate_html.py
+++ bin/generate_html.py
@@ -1,17 +1,26 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+# https://www.shadertoy.com/media/shaders/tlBcD1.jpg
+
 import codecs
 import json
 import glob
 
 import jinja2
 
+# Jinja2 Template system
 templateEnv = jinja2.Environment(
     loader=jinja2.FileSystemLoader(searchpath="./templates/")
 )
 template = templateEnv.get_template("index.html")
 
-data = [json.load(codecs.open(d, encoding="utf-8")) for d in glob.glob("./data/**")]
+# Use 'started' date to sort from latest to oldest
+data = sorted(
+    [json.load(codecs.open(d, encoding="utf-8")) for d in glob.glob("./data/**")],
+    key=lambda a: a["started"],
+    reverse=True,
+)
 
-with codecs.open("index.html", "w", "utf-8") as fh:
-    fh.write(template.render(events=data))
+# No more code, only template from jinja2
+with codecs.open("index.html", "w", "utf-8") as outFile:
+    outFile.write(template.render(events=data))
