896c2251e9f524c47e76a386d723f5013f73a163
Jochen Kupperschmidt <homework@nwsnet.de>
2021-06-24 14:35:00 +0200

    Use `Path`, `load_json` in `turn_to_int` script

--- bin/turn_to_int.py
+++ bin/turn_to_int.py
@@ -1,14 +1,20 @@
 import json
-import glob
-import codecs
+from pathlib import Path
+import sys
 
-files = glob.glob('data/*.json')
-for f in files :
-    data = json.load(codecs.open(f,'r','utf-8'))
+sys.path.append('.')
+from generator.files import load_json
+
+
+for path in Path('data').glob('*.json'):
+    data = load_json(path)
+
     for p in data['phases']:
         for e in p['entries']:
             if e.get('tic80_cart_id'):
                 e['tic80_cart_id'] = int(e['tic80_cart_id'])
             e['source_file'] = e.get('shader_file')
             del e['shader_file']
-    json.dump(data,codecs.open(f,'w','utf-8'),indent=4)
+
+    with path.open(encoding='utf-8') as f:
+        json.dump(data, indent=4)
