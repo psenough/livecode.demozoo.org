70724aa11ec32d6cbca476b35599160120d94eb6
Filipe Cruz <psenough@gmail.com>
2021-10-05 13:36:19 +0100

    no message

--- public/shader_file_sources/2021_09_lovebyte_byte_battle/blackle_first_round.lua
+++ public/shader_file_sources/2021_09_lovebyte_byte_battle/blackle_first_round.lua
@@ -1,15 +1,15 @@
-function TIC()cls()t=time()/999s=math.sin
-l={}p=pairs
-for x=0,20 do
-l[x]={x=s(x*7+t)*250,y=s(x+t)*299}end
-for i,v in p(l)do
-m=1e9w=v
-for q,r in p(l)do
-n=(r.x-v.x)^2+(r.y-v.y)^2
-if (n-q)^2<m then
+function TIC()cls()t=time()/3e3l={}s=math.sin
+t=t+s(t*8)/9
+for i=1,40 do
+l[i]={x=s(t+i)*239,y=s(t+i*97)*149}end
+for i=1,40 do
+v=l[i]m=1e9w=v
+for j=1,40 do
+p=l[j]n=(p.x-v.x)^2+(p.y-v.y)^2
+if n<m==(n>9) then
 m=n
-w=r
+w=p
 end
 end
-line(v.x,v.y,w.x,w.y,i%5)end
+line(w.x,w.y,v.x,v.y,i%4+8)end
 end
