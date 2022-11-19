a3a2d4f5fdc997b1e7442875fade819fc80fa04d
David Sims <thedavesims@users.noreply.github.com>
2022-11-19 12:41:34 +0000

    Removed unnecessary TIC-80 chunks from lua

--- public/shader_file_sources/2022_11_14_fieldfx_bbc/bytebattle/dave84.lua
+++ public/shader_file_sources/2022_11_14_fieldfx_bbc/bytebattle/dave84.lua
@@ -13,13 +13,3 @@ end
 m=math
 s=m.sin
 c=m.cos
--- <WAVES>
--- 000:00000000ffffffff00000000ffffffff
--- 001:0123456789abcdeffedcba9876543210
--- 002:0123456789abcdef0123456789abcdef
--- </WAVES>
-
--- <PALETTE>
--- 000:1a1c2c5d275db13e53ef7d57ffcd75a7f07038b76425717929366f3b5dc941a6f673eff7f4f4f494b0c2566c86333c57
--- </PALETTE>
-
