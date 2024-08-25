11c3cd5e7b98bf693ff7ca7941ebbc5f3a9a5739
Matthieu Totet <matthieu.totet@gmail.com>
2024-08-25 16:42:03 +0200

    Evoke 2024 Bytewall

---
+++ public/shader_file_sources/2024_08_16_byte_jam_byte_wall_evoke/trifox2.lua
@@ -0,0 +1,26 @@
+-- Welcome to the Evoke ByteWall!
+-- Please delete this code and play.
+--
+-- Any issues? Find Violet =)
+--
+-- Have fun!
+-- /jtruk + /VioletRaccoon
+
+function TIC()
+	local t=time()*.001
+	for y=0,135 do
+		for x=0,239 do
+			local dx=120-x
+			local dy=68-y
+			local d=(dx^12+dy^12)^.5
+			local a=math.atan2(dy,dx)
+			pix(x,y,18+math.sin(d*.1+a-t)*3)
+		end
+	end
+
+	local text="ByteWall!"
+	local x=50
+	local y=75-math.abs(math.sin(t*3)*30)
+	print(text,x+1,y+1,15,false,3)
+	print(text,x,y,12,false,3)
+end
